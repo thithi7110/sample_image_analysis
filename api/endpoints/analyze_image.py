c08211bf3f891f146743d356cd168f776499a7ed
thithi7110 <thithi7110@gmail.com>
2024-02-17 08:19:19 +0900

    fix unixtimestamp

--- api/endpoints/analyze_image.py
+++ api/endpoints/analyze_image.py
@@ -4,6 +4,8 @@ from services.analyze_image_service import AnalyzeImage
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
+from utils.date_util import get_jst_unix_timestamp
+
 router = APIRouter()
 
 # リクエストボディのモデル定義
@@ -14,16 +16,18 @@ class ImagePathRequest(BaseModel):
 @router.post("/")
 async def analyze_image(request: ImagePathRequest,
     repository: AIAnalysisLogRepository = Depends(get_ai_analysis_log_repository)):
+    start_time = get_jst_unix_timestamp()
     analyze_image = AnalyzeImage()
     image_description_response = analyze_image.analyze(request.image_path)
+    end_time = get_jst_unix_timestamp()
     repository.create_ai_analysis_log(
         image_path=request.image_path,
         success=image_description_response.success,
         message=image_description_response.message,
         class_=image_description_response.estimated_data.get("class"),
         confidence=image_description_response.estimated_data.get("confidence"),
-        request_timestamp=0,
-        response_timestamp=0
+        request_timestamp=start_time,
+        response_timestamp=end_time
     )
     
     return image_description_response
